b4126bba64461a2e2a4b292fb744739ecffb340c
RebecaHndz <113383261+RebecaHndz@users.noreply.github.com>
2022-09-12 13:48:51 -0600

    Add files via upload

---
+++ Periodo3clase4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+float calcularPerimetro(float); //prototipo de funcion
+
+int main(){
+	double lado;
+	cout << "Ingrese el lado del cuadrado: " << endl;
+	cin>> lado;
+	cout << " El perimetro del cuadrado es: " << calcularPerimetro(lado);
+	return 0;
+}
+float calcularPerimetro(float lado){
+	double perimetro = lado *4;
+	return perimetro;
+}
---
+++ Periodo3clase6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ int main()
+ {
+ 	int numero;
+ 	do
+ 	{
+ 		cout<< "ingrese un numero ";
+ 		cin >> numero;
+	 }
+	 while(numero<= 100);
+	 system("PAUSE");
+	 return 0;
+ }
